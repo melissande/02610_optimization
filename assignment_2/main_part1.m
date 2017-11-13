742d493fafec8e6cfd9e1588e22fd921619e520a
Melissande <melissande@melissandes-air.eduroam.wireless.dtu.dk>
2017-11-13 15:43:42 +0100

    part1_123

--- assignment_2/main_part1.m
+++ assignment_2/main_part1.m
@@ -22,6 +22,7 @@ ylabel('y')
 n=length(y_true);
 A=[ti,ones(n,1)];
 b=y_obs;
+p=2;
 
 
 %% Q2: l2 Squares Estimation
@@ -35,6 +36,7 @@ x_ls=-inv(H)*g;
 y_ls=A*x_ls;
 figure(2)
 plot(ti,y_obs,'bo',ti,y_true,'r',ti,y_ls,'g');
+title('Least Square Solution')
 legend('Observations','True Model','Least Square Solution')
 xlabel('t')
 ylabel('y')
@@ -42,6 +44,7 @@ ylabel('y')
 %Histogram with outliers
 res_ls=y_obs-y_ls;
 figure(3)
+subplot(1,2,1)
 hist(res_ls,50);
 title('Histogram of the errors for Least Square Solution')
 %Histogram without outliers
@@ -49,9 +52,10 @@ idx=find(res_ls>12);
 res_ls_wo=res_ls;
 res_ls_wo(idx)=[];
 n2=length(res_ls_wo);
-p=2;
 
-figure(4)
+
+
+subplot(1,2,2)
 hist(res_ls_wo,50);
 title('Histogram of the errors for Least Square Solution without outliers')
 
@@ -60,7 +64,7 @@ title('Histogram of the errors for Least Square Solution without outliers')
 %Predictions
 std_noise=sqrt(sum(res_ls_wo.^2)/length(res_ls_wo));
 PI_data_ls=y_ls+tinv([0.025  0.975],n2-p)*std_noise;
-figure(5)
+figure(4)
 plot(ti,y_obs,'bo',ti,y_true,'r',ti,y_ls,'g',ti,PI_data_ls(:,1),'--g',ti,PI_data_ls(:,2),'--g');
 legend('Observations','True Model','Least Square Solution','Low Bound PI',' High Bound PI')
 xlabel('t')
@@ -73,5 +77,57 @@ PI_param_ls=x_ls+tinv([0.025  0.975],p)*std_param;
 disp('LS: PI for parameters')
 disp(PI_param_ls)
 
+
 %% Q3: l1  Estimation
+
+f_lp=[zeros(p,1);ones(n,1)];
+A_lp=[-A,-eye(n);A,-eye(n)];
+b_lp=[-b;b];
+  
+x_l1 = linprog(f_lp,A_lp,b_lp); 
+x_l1=x_l1(1:2);
+y_l1=A*x_l1;
+
+figure(5)
+plot(ti,y_obs,'bo',ti,y_true,'r',ti,y_l1,'g');
+legend('Observations','True Model','L1 Estimate')
+title('L1 Estimate')
+xlabel('t')
+ylabel('y')
+
+%Histogram with outliers
+res_l1=y_obs-y_l1;
+figure(6)
+subplot(1,2,1)
+hist(res_l1,50);
+title('Histogram of the errors for L1 Estimate Solution')
+%Histogram without outliers
+idx=find(res_l1>12);
+res_l1_wo=res_l1;
+res_l1_wo(idx)=[];
+n2=length(res_l1_wo);
+
+
+subplot(1,2,2)
+hist(res_l1_wo,50);
+title('Histogram of the errors for L1 Estimate without outliers')
+
+%Goodness of the fit
+
+%Predictions
+std_noise=sqrt(sum(res_l1_wo.^2)/length(res_l1_wo));
+PI_data_l1=y_l1+tinv([0.025  0.975],n2-p)*std_noise;
+
+figure(7)
+plot(ti,y_obs,'bo',ti,y_true,'r',ti,y_l1,'g',ti,PI_data_l1(:,1),'--g',ti,PI_data_l1(:,2),'--g');
+legend('Observations','True Model','L1 Estimate','Low Bound PI',' High Bound PI')
+xlabel('t')
+ylabel('y')
+
+%Parameters
+cov_param=cov(x_l1);
+std_param=diag(sqrt(cov_param));
+PI_param_l1=x_l1+tinv([0.025  0.975],p)*std_param;
+disp('L1: PI for parameters')
+disp(PI_param_l1)
 
