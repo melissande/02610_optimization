62561be0a944422cbd16b08b58209360195824e9
Phillip Vetter <Phil-sunshine@hotmail.com>
2017-11-25 13:23:59 +0100

    changes to main1

--- assignment_2/main_part1.m
+++ assignment_2/main_part1.m
@@ -1,4 +1,7 @@
 %% PROBLEM 1
+close all
+clear all
+clc
 
 %% Q1: PLOT DATA
 
@@ -36,7 +39,7 @@ x_ls=-H\g;
 y_ls=A*x_ls;
 figure(2)
 plot(ti,y_obs,'bo',ti,y_true,'r',ti,y_ls,'g');
-title('Least Square Solution')
+title('Least Square Solution with Outliers')
 legend('Observations','True Model','Least Square Solution')
 xlabel('t')
 ylabel('y')
@@ -46,7 +49,7 @@ res_ls=y_obs-y_ls;
 figure(3)
 subplot(1,2,1)
 hist(res_ls,50);
-title('Histogram of the errors for Least Square Solution')
+title({'Histogram of the Errors for', 'Least Square Solution with Outliers'})
 
 %Histogram without outliers
 idx=find(res_ls>12);
@@ -56,26 +59,66 @@ n2=length(res_ls_wo);
 
 subplot(1,2,2)
 hist(res_ls_wo,50);
-title('Histogram of the errors for Least Square Solution without outliers')
+title({'Histogram of the Errors for','Least Square Solution without Outliers'})
+set(gcf,'units','points','position',[10,10,1000,350])
+
+%Define new variables and remove outliers from this data
+ti_wo = ti;
+y_obs_wo = y_obs;
+y_ls_wo = y_ls;
+y_true_wo = y_true;
+
+ti_wo(idx)=[];
+y_obs_wo(idx)=[];
+y_ls_wo(idx)=[];
+y_true_wo(idx)=[];
 
 %Goodness of the fit
 
 %Predictions
-std_noise = sqrt(sum(res_ls_wo.^2)/n2);
-PI_data_ls = y_ls + tinv([0.025  0.975],n2-p) * std_noise;
+std_noise = sqrt(sum(res_ls_wo.^2)/(n2-p));
+PI_data_ls = y_ls_wo + tinv([0.025  0.975],n2-p) * std_noise;
+
+%Investigation additional prediction interval methods. The former yields
+%the same result and the latter yields a very narrow interval. They have
+%been plotted to compare.
+
+%{
+PI_data_ls_wo = y_ls_wo + tinv([0.025  0.975],n2-p-2) .* (std_noise * sqrt( 1 + 1/(n2-p) + (ti_wo-mean(ti_wo)).^2 / sum( (ti_wo-mean(ti_wo)).^2 ) ) );
+
+for i = 1:n2
+PI_data_ls_wo2(i,:) = y_ls_wo(i) + tinv([0.025 0.975],n2-p) * ( [ti_wo(i); 1]'*inv(H)*[ti_wo(i); 1] );
+end
+
+figure(5)
+plot(ti_wo,y_true_wo,'r',ti_wo,y_ls_wo,'g',ti_wo,PI_data_ls(:,1),'--g',ti_wo,PI_data_ls(:,2),'--g')
+figure(6)
+plot(ti_wo,y_true_wo,'r',ti_wo,y_ls_wo,'g',ti_wo,PI_data_ls_wo(:,1),'--g',ti_wo,PI_data_ls_wo(:,2),'--g')
+figure(7)
+plot(ti_wo,y_true_wo,'r',ti_wo,y_ls_wo,'g',ti_wo,PI_data_ls_wo2(:,1),'--g',ti_wo,PI_data_ls_wo2(:,2),'--g')
+%}
+
 figure(4)
-plot(ti,y_obs,'bo',ti,y_true,'r',ti,y_ls,'g',ti,PI_data_ls(:,1),'--g',ti,PI_data_ls(:,2),'--g');
+plot(ti_wo,y_obs_wo,'bo',ti_wo,y_true_wo,'r',ti_wo,y_ls_wo,'g',ti_wo,PI_data_ls(:,1),'--g',ti_wo,PI_data_ls(:,2),'--g');
+title('Least Squares Solution without Outliers including Prediction Interval')
 legend('Observations','True Model','Least Square Solution','Low Bound PI',' High Bound PI')
 xlabel('t')
 ylabel('y')
 
-%Parameters
-cov_param=cov(x_ls);
-std_param=diag(sqrt(cov_param));
-PI_param_ls=x_ls+tinv([0.025  0.975],p)*std_param;
-disp('LS: PI for parameters')
-disp(PI_param_ls)
 
+%Confidence Interval for the Parameters in x
+cov_param = std_noise^2*inv(H);
+std_param = diag(sqrt(cov_param));
+for i = 1:2
+    confint_param_ls(i,:) = x_ls(i)+tinv([0.025  0.975],p)*std_param(i);
+end
+disp('LS: Confidence Interval for Parameters')
+disp(confint_param_ls)
+
+T = table(x_ls, tinv(0.975,p)*std_param, std_param, corrcov(cov_param));
+T.Properties.VariableNames = {'Estimate','ConfidenceInterval','Variance','CorrelationMatrix'};
+T.Properties.RowNames = {'x1','x2'};
+disp(T)
 
 %% Q3: l1  Estimation
 
@@ -100,6 +143,7 @@ figure(6)
 subplot(1,2,1)
 hist(res_l1,50);
 title('Histogram of the errors for L1 Estimate Solution')
+
 %Histogram without outliers
 idx=find(res_l1>12);
 res_l1_wo=res_l1;
