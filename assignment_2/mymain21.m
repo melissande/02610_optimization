09fe88a379f3322bb85aefd5992f1c3779d0cfcd
Phillip Vetter <Phil-sunshine@hotmail.com>
2017-12-03 11:15:06 +0100

    main21 update

--- assignment_2/mymain21.m
+++ assignment_2/mymain21.m
@@ -29,5 +29,36 @@ legend('Data','Location','northwest')
 set(gcf,'units','points','position',[10,10,1250,350])
 set(findobj(gcf,'type','axes'),'TickLabelInterpreter','Latex')
 
+%% Linear least squares solution
+
+%Solve the linear system of equations
+
+A = [1./x, ones(length(x),1)];
+b = 1./y;
+
+x_star = A\b;
+
+%Compute the coefficients by substitution into the model
+
+theta_ls(1) = inv(x_star(1));
+theta_ls(2) = x_star(2)*theta_ls(1);
+
+%% Plotting the model prediction against the data
+
+%Constructing model data from least squares estimate
+x_new = 0.0:0.01:2.5;
+y_model = ( theta_ls(1)*x_new ) ./ (theta_ls(2)+x_new);
+
+figure('DefaultAxesFontSize',16)
+plot(x,y,'bo',x_new,y_model,'r','LineWidth',1.2);
+title('Reaction Rate $y$ vs Substrate Concentration $x$','Interpreter','Latex')
+legend({'Data','Model Prediction'},'Location','northwest')
+xlabel('Substrate Concentration $x$','Interpreter','Latex')
+ylabel('Reaction Rate $y$','Interpreter','Latex')
+set(gca,'TickLabelInterpreter','Latex')
+
+
+
+
 
 
