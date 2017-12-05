909bbc383be80d8f08fb34c2b51bd78e44010ed6
Phillip Vetter <Phil-sunshine@hotmail.com>
2017-12-05 14:31:10 +0100

    main21_changes

--- assignment_2/main_part2.m
+++ assignment_2/main_part2.m
@@ -9,25 +9,32 @@ clc
 x = [2,2,0.667,0.667,0.4,0.4,0.286,0.286,0.222,0.220,0.2,0.2]';
 y = [0.0615,0.0527,0.0334,0.0334,0.0138,0.0258,0.0129,0.0183,0.0083,0.0169,0.0129,0.0087]';
 
+invx = 1./x;
+b = 1./y;
+
 %% Plotting (x,y) and (1/x,1/y)
 
 figure('DefaultAxesFontSize',16)
-subplot(1,2,1)
 plot(x,y,'bo');
 title('Reaction Rate $y$ vs Substrate Concentration $x$','Interpreter','Latex')
 legend('Data','Location','northwest')
 xlabel('Substrate Concentration $x$','Interpreter','Latex')
 ylabel('Reaction Rate $y$','Interpreter','Latex')
 set(gca,'TickLabelInterpreter','Latex')
+set(gcf,'units','points','position',[10,10,900,450])
+xlim([0 2.05])
 
-subplot(1,2,2)
-plot(1./x,1./y,'bo')
+figure('DefaultAxesFontSize',16)
+plot(invx,b,'bo')
 title('Inv. Reaction Rate $\frac{1}{y}$ vs Inv. Substrate Concentration $\frac{1}{x}$','Interpreter','Latex')
 xlabel('Inv. Substrate Concentration $\frac{1}{x}$','Interpreter','Latex')
 ylabel('Inv. Reaction Rate $\frac{1}{y}$','Interpreter','Latex')
 legend('Data','Location','northwest')
-set(gcf,'units','points','position',[10,10,1250,350])
-set(findobj(gcf,'type','axes'),'TickLabelInterpreter','Latex')
+%set(gcf,'units','points','position',[10,10,500,12500])
+%set(findobj(gcf,'type','axes'),'TickLabelInterpreter','Latex')
+set(gcf,'units','points','position',[10,10,900,450])
+set(gca,'TickLabelInterpreter','Latex')
+xlim([0 5.1])
 
 %% Linear least squares solution
 
--- assignment_2/mymain23.m
+++ assignment_2/mymain23.m
@@ -18,6 +18,15 @@ xlim([-5 205])
 ylim([-0.5 10.5])
 
 %% 
+%Solve the differential equation using ode45 and calling the diff-equation. 
+%The last input is the parameter values p of @diffmodel
 
-[T,X] = ode45(@diffmodel,[0:10:200],10,[],[3,5])
+[T,X] = ode45(@diffmodel,[0:10:200],10,[],[3,5]);
+
+%% Contour Plot
+
+%We feed various inputs of p to the above solution to the ode to generate
+%the various y(theta,t). Phi can then be computed
+
+
 
